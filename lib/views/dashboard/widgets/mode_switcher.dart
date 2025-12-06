d880873c31a843a8eda0114a42700e428125b4a5
tianquege <wangn5635@gmail.com>
2025-12-06 09:24:57 +0800

    feat: add tun mode switch to ModeSwitcher on Windows

--- lib/views/dashboard/widgets/mode_switcher.dart
+++ lib/views/dashboard/widgets/mode_switcher.dart
@@ -4,6 +4,7 @@ import 'package:fl_clash/state.dart';
 import 'package:fl_clash/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fl_clash/common/common.dart';
 
 class ModeSwitcher extends ConsumerWidget {
   const ModeSwitcher({super.key});
@@ -17,32 +18,59 @@ class ModeSwitcher extends ConsumerWidget {
     return CommonCard(
       child: Padding(
         padding: const EdgeInsets.all(12),
-        child: SegmentedButton<Mode>(
-          segments: [
-            ButtonSegment<Mode>(
-              value: Mode.rule,
-              label: const Text('智能'),
-              icon: const Icon(Icons.playlist_add_check),
-            ),
-            ButtonSegment<Mode>(
-              value: Mode.global,
-              label: const Text('全局'),
-              icon: const Icon(Icons.public),
-            ),
-          ],
-          selected: {mode == Mode.direct ? Mode.rule : mode},
-          onSelectionChanged: (Set<Mode> newSelection) {
-            if (newSelection.isEmpty) return;
-            final selectedMode = newSelection.first;
-            globalState.appController.changeMode(selectedMode);
-          },
-          style: ButtonStyle(
-            padding: WidgetStateProperty.all(
-              const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: SegmentedButton<Mode>(
+              segments: [
+                ButtonSegment<Mode>(
+                  value: Mode.rule,
+                  label: const Text('智能'),
+                  icon: const Icon(Icons.playlist_add_check),
+                ),
+                ButtonSegment<Mode>(
+                  value: Mode.global,
+                  label: const Text('全局'),
+                  icon: const Icon(Icons.public),
+                ),
+              ],
+              selected: {mode == Mode.direct ? Mode.rule : mode},
+              onSelectionChanged: (Set<Mode> newSelection) {
+                if (newSelection.isEmpty) return;
+                final selectedMode = newSelection.first;
+                globalState.appController.changeMode(selectedMode);
+              },
+              style: ButtonStyle(
+                padding: WidgetStateProperty.all(
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                ),
+              ),
             ),
           ),
-        ),
+          if (system.isWindows) ...[
+            const SizedBox(width: 12),
+            const Text(
+              "开启虚拟网卡",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Transform.scale(
+              scale: 0.8,
+              child: Switch(
+                value: ref.watch(
+                  patchClashConfigProvider.select((state) => state.tun.enable),
+                ),
+                onChanged: (value) {
+                  ref.read(patchClashConfigProvider.notifier).updateState(
+                        (state) => state.copyWith.tun(enable: value),
+                      );
+                },
+              ),
+            ),
+          ],
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
